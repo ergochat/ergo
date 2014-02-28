8c8d371ae2d9274ed609535183b69e1ada2d5853
Jeremy Latt <jeremy.latt@gmail.com>
2014-02-27 20:42:12 -0800

    don't give operator to the first person to enter an empty persistent channel

--- irc/channel.go
+++ irc/channel.go
@@ -145,10 +145,8 @@ func (channel *Channel) Join(client *Client, key string) {
 
 	client.channels.Add(channel)
 	channel.members.Add(client)
-	if len(channel.members) == 1 {
-		if !channel.flags[Persistent] {
-			channel.members[client][ChannelCreator] = true
-		}
+	if !channel.flags[Persistent] && (len(channel.members) == 1) {
+		channel.members[client][ChannelCreator] = true
 		channel.members[client][ChannelOperator] = true
 	}
 
