b6feca05a335852142b1fb41cafeb2a1ec4a05db
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2019-02-21 03:29:39 -0500

    first pass at #409

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$GOPATH" ]; then
+	echo \$GOPATH is unset: see https://golang.org/doc/code.html for details
+	exit 1
+fi
+
+EXPECTED_DIR=${GOPATH}/src/github.com/oragono/oragono
+
+if [ `pwd` != "$EXPECTED_DIR" ] ; then
+	echo working checkout is not where \$GOPATH expects it: should be $EXPECTED_DIR
+	exit 1
+fi
+
+go install -v
+echo successfully installed as ${GOPATH}/bin/oragono
