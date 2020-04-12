11d1d96d71e427771b7f52a6c3837a758a036fbc
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2020-04-12 13:45:24 -0400

    align up to 10

--- irc/logger/logger.go
+++ irc/logger/logger.go
@@ -229,7 +229,7 @@ func (logger *singleLogger) Log(level Level, logType string, messageParts ...str
 	var rawBuf bytes.Buffer
 	// XXX magic number here: 9 is len("listeners"), the longest log category name
 	// in current use. it's not a big deal if this number gets out of date.
-	fmt.Fprintf(&rawBuf, "%s : %-5s : %-9s : ", time.Now().UTC().Format("2006-01-02T15:04:05.000Z"), LogLevelDisplayNames[level], logType)
+	fmt.Fprintf(&rawBuf, "%s : %-5s : %-10s : ", time.Now().UTC().Format("2006-01-02T15:04:05.000Z"), LogLevelDisplayNames[level], logType)
 	for i, p := range messageParts {
 		rawBuf.WriteString(p)
 
