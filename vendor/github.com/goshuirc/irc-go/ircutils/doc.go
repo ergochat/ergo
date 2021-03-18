074a5a077e8cb15a6eafb14e7ff001e952c63b6e
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2021-03-18 03:56:57 -0400

    bump irc-go to include new ircutils function

---
+++ vendor/github.com/goshuirc/irc-go/ircutils/doc.go
@@ -0,0 +1,9 @@
+// written by Daniel Oaks <daniel@danieloaks.net>
+// released under the ISC license
+
+/*
+Package ircutils provides small, useful utility functions and classes.
+
+This package is in an alpha stage.
+*/
+package ircutils
