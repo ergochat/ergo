76fa365a7a0b79ea62b96c9d69250ba2330b49e2
Karsten Højgaard <karsten@here.dk>
2021-05-28 23:15:30 +0200

    Update run.sh to use newly renamed ergo binaries

--- distrib/docker/run.sh
+++ distrib/docker/run.sh
@@ -10,7 +10,7 @@ if [ ! -f "/ircd/ircd.yaml" ]; then
     # change default oper passwd
     OPERPASS=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c20)
     echo "Oper username:password is admin:$OPERPASS"
-    ENCRYPTEDPASS=$(echo "$OPERPASS" | /ircd-bin/oragono genpasswd)
+    ENCRYPTEDPASS=$(echo "$OPERPASS" | /ircd-bin/ergo genpasswd)
     ORIGINALPASS='\$2a\$04\$0123456789abcdef0123456789abcdef0123456789abcdef01234'
 
     awk "{gsub(/password: \\\"$ORIGINALPASS\\\"/,\"password: \\\"$ENCRYPTEDPASS\\\"\")}1" /tmp/ircd.yaml > /tmp/ircd2.yaml
@@ -23,7 +23,7 @@ if [ ! -f "/ircd/ircd.yaml" ]; then
 fi
 
 # make self-signed certs if they don't already exist
-/ircd-bin/oragono mkcerts
+/ircd-bin/ergo mkcerts
 
 # run!
-exec /ircd-bin/oragono run
+exec /ircd-bin/ergo run
