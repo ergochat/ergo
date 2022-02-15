197a9d4b5eaabb1feb30dfc4db26570858634d4e
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2022-02-15 10:39:38 -0500

    use exact integer parsing for znc.in/playback

--- irc/misc_test.go
+++ irc/misc_test.go
@@ -12,6 +12,11 @@ func TestZncTimestampParser(t *testing.T) {
 	assertEqual(zncWireTimeToTime("1558338348.988"), time.Unix(1558338348, 988000000).UTC(), t)
 	assertEqual(zncWireTimeToTime("1558338348.9"), time.Unix(1558338348, 900000000).UTC(), t)
 	assertEqual(zncWireTimeToTime("1558338348"), time.Unix(1558338348, 0).UTC(), t)
+	assertEqual(zncWireTimeToTime("1558338348.99999999999999999999999999999"), time.Unix(1558338348, 999999999).UTC(), t)
+	assertEqual(zncWireTimeToTime("1558338348.999999999111111111"), time.Unix(1558338348, 999999999).UTC(), t)
+	assertEqual(zncWireTimeToTime("1558338348.999999991111111111"), time.Unix(1558338348, 999999991).UTC(), t)
 	assertEqual(zncWireTimeToTime(".988"), time.Unix(0, 988000000).UTC(), t)
+	assertEqual(zncWireTimeToTime("0"), time.Unix(0, 0).UTC(), t)
 	assertEqual(zncWireTimeToTime("garbage"), time.Unix(0, 0).UTC(), t)
+	assertEqual(zncWireTimeToTime(""), time.Unix(0, 0).UTC(), t)
 }
