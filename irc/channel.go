ergochat/ergo

commit 38a6d17ee5ce6e1096c3dfd6d11f6f35d9a71ca6

clean up nested batch logic

irc/channel.go
@@ -1059,7 +1059,7 @@ func (channel *Channel) replayHistoryItems(rb *ResponseBuffer, items []history.I
 		}
 	}
 
-	batchID := rb.StartNestedHistoryBatch(chname)
+	batchID := rb.StartNestedBatch(chname, "chathistory")
 	defer rb.EndNestedBatch(batchID)
 
 	for _, item := range items {

irc/client.go
@@ -850,7 +850,7 @@ func (client *Client) replayPrivmsgHistory(rb *ResponseBuffer, items []history.I
 	if target == "" {
 		target = nick
 	}
-	batchID = rb.StartNestedHistoryBatch(target)
+	batchID = rb.StartNestedBatch(target, "chathistory")
 
 	isSelfMessage := func(item *history.Item) bool {
 		// XXX: Params[0] is the message target. if the source of this message is an in-memory

irc/caps/constants.go
@@ -62,6 +62,8 @@ const (
 	RelaymsgTagName = "draft/relaymsg"
 	// BOT mode: https://ircv3.net/specs/extensions/bot-mode
 	BotTagName = "bot"
+	// https://ircv3.net/specs/extensions/chathistory
+	ChathistoryTargetsBatchType = "draft/chathistory-targets"
 )
 
 func init() {

irc/handlers.go
@@ -655,10 +655,8 @@ func chathistoryHandler(server *Server, client *Client, msg ircmsg.Message, rb *
 		} else {
 			// successful responses are sent as a chathistory or history batch
 			if listTargets {
-				if rb.session.capabilities.Has(caps.Batch) { // #2066
-					batchID := rb.StartNestedBatch("draft/chathistory-targets")
-					defer rb.EndNestedBatch(batchID)
-				}
+				batchID := rb.StartNestedBatch(caps.ChathistoryTargetsBatchType)
+				defer rb.EndNestedBatch(batchID)
 				for _, target := range targets {
 					name := server.UnfoldName(target.CfName)
 					rb.Add(nil, server.name, "CHATHISTORY", "TARGETS", name,

irc/responsebuffer.go
@@ -193,6 +193,9 @@ func (rb *ResponseBuffer) sendBatchEnd(blocking bool) {
 // Starts a nested batch (see the ResponseBuffer struct definition for a description of
 // how this works)
 func (rb *ResponseBuffer) StartNestedBatch(batchType string, params ...string) (batchID string) {
+	if !rb.session.capabilities.Has(caps.Batch) {
+		return
+	}
 	batchID = rb.session.generateBatchID()
 	msgParams := make([]string, len(params)+2)
 	msgParams[0] = "+" + batchID
@@ -217,19 +220,6 @@ func (rb *ResponseBuffer) EndNestedBatch(batchID string) {
 
 	rb.nestedBatches = rb.nestedBatches[0 : len(rb.nestedBatches)-1]
 	rb.AddMessage(ircmsg.MakeMessage(nil, rb.target.server.name, "BATCH", "-"+batchID))
-}
-
-// Convenience to start a nested batch for history lines, at the highest level
-// supported by the client (`history`, `chathistory`, or no batch, in descending order).
-func (rb *ResponseBuffer) StartNestedHistoryBatch(params ...string) (batchID string) {
-	var batchType string
-	if rb.session.capabilities.Has(caps.Batch) {
-		batchType = "chathistory"
-	}
-	if batchType != "" {
-		batchID = rb.StartNestedBatch(batchType, params...)
-	}
-	return
 }
 
 // Send sends all messages in the buffer to the client.

commit f100c1d0fa1025b40fb09b0e51e0e703ff4b3e59

fix incorrect chathistory batch types
This was introduced in 38a6d17ee5ce6e1096c3dfd6d11f6f35d9a71ca6

irc/channel.go
@@ -1059,7 +1059,7 @@ func (channel *Channel) replayHistoryItems(rb *ResponseBuffer, items []history.I
 		}
 	}
 
-	batchID := rb.StartNestedBatch(chname, "chathistory")
+	batchID := rb.StartNestedBatch("chathistory", chname)
 	defer rb.EndNestedBatch(batchID)
 
 	for _, item := range items {

irc/client.go
@@ -850,7 +850,7 @@ func (client *Client) replayPrivmsgHistory(rb *ResponseBuffer, items []history.I
 	if target == "" {
 		target = nick
 	}
-	batchID = rb.StartNestedBatch(target, "chathistory")
+	batchID = rb.StartNestedBatch("chathistory", target)
 
 	isSelfMessage := func(item *history.Item) bool {
 		// XXX: Params[0] is the message target. if the source of this message is an in-memory